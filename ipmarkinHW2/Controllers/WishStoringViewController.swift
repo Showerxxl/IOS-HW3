1c602053d6205e0c86c5b8bde3e83fb668894c69
Showerxxl <mark1n.igor@yandex.ru>
2024-11-28 17:11:03 +0300

    fourth task

--- ipmarkinHW2/Controllers/WishStoringViewController.swift
+++ ipmarkinHW2/Controllers/WishStoringViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 final class WishStoringViewController: UIViewController {
-    
     private let table: UITableView = UITableView(frame: .zero)
+    private var wishArray: [String] = ["I wish to add cells to the table"]
     override func viewDidLoad() {
         view.backgroundColor = .blue
         configureTable()
@@ -13,15 +13,29 @@ final class WishStoringViewController: UIViewController {
         table.separatorStyle = .none
         table.layer.cornerRadius = Constants.tableCornerRadius
         table.pin(to: view, Constants.tableOffset)
+        table.register(WrittenWishCell.self, forCellReuseIdentifier: WrittenWishCell.reuseId)
     }
+    
+    
+    
 }
 
 extension WishStoringViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return wishArray.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: WrittenWishCell.reuseId,
+            for: indexPath
+        )
+        guard let wishCell = cell as? WrittenWishCell else { return cell }
+        
+        wishCell.configure(with: wishArray[indexPath.row])
+        
+        return wishCell
     }
 }
